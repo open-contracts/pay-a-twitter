bb2d16b88eec45e63da69709d78038c4f9e2c309
open-contracts <90533597+open-contracts@users.noreply.github.com>
2021-11-01 21:49:15 -0700

    Update contract.sol

--- contract.sol
+++ contract.sol
@@ -3,14 +3,20 @@ pragma solidity ^0.8.0;
 import "https://github.com/open-contracts/protocol/blob/main/solidity_contracts/OpenContractRopsten.sol";
 
 contract FiatSwap is OpenContractAlpha {
-
-    mapping(bytes32 => address) beneficiary;
+    
+    mapping(bytes32 => address) seller;
+    mapping(bytes32 => address) buyer;
     mapping(bytes32 => uint256) amount;
+    mapping(bytes32 => uint256) lockedUntil;
 
 
-    function EthForHash(bytes32 txHash) public view returns(uint256) {
-        require(msg.sender == beneficiary[txHash], "No Eth offered for you.");
-        return amount[txHash];
+    function secondsLeft(bytes32 offerHash) public view returns(int256) {
+        return int256(lockedUntil[offerHash]) - int256(block.timestamp);
+    }
+
+    function ethOffered(bytes32 offerHash) public view returns(uint256) {
+        require(msg.sender == buyer[offerHash], "No ETH offered for you.");
+        return amount[offerHash];
     }
 
 
@@ -18,16 +24,25 @@ contract FiatSwap is OpenContractAlpha {
         return keccak256(abi.encodePacked(sellerVenmo, priceInCent, transactionMessage, secret));
     }
 
-    function venmoPurchase(bytes32 oracleHash, address payable msgSender, bytes32 txHash) 
+    function venmoPurchase(bytes32 oracleHash, address payable msgSender, bytes32 offerHash) 
     public _oracle(oracleHash, msgSender, this.venmoPurchase.selector) returns(bool) {
-        require(beneficiary[txHash] == msgSender);
-        uint256 reward = amount[txHash];
-        amount[txHash] = 0;
-        return msgSender.send(reward);
+        require(buyer[offerHash] == msgSender);
+        uint256 payment = amount[offerHash];
+        amount[offerHash] = 0;
+        return msgSender.send(payment);
     }
 
-    function sell(address buyer, bytes32 txHash) public payable {
-        amount[txHash] = msg.value;
-        beneficiary[txHash] = buyer;
+    function offer(address to, bytes32 offerHash, uint256 lockForSeconds) public payable {
+        amount[offerHash] = msg.value;
+        buyer[offerHash] = to;
+        lockedUntil[offerHash] = block.timestamp + lockForSeconds;
+        seller[offerHash] = msg.sender;
+    }
+    
+    function retract(bytes32 offerHash) public returns(bool) {
+        require(seller[offerHash] == msg.sender, 'only seller can retract offer');
+        uint256 payment = amount[offerHash];
+        amount[offerHash] = 0;
+        return payable(msg.sender).send(payment);
     }
 }
