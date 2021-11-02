6134478a693f1e8d5bf3899f4fbc553550a16bd8
Jonas Metzger <metzgerj@stanford.edu>
2021-11-01 21:13:23 -0700

    proof of concept

--- contract.sol
+++ contract.sol
@@ -1,17 +1,30 @@
-pragma solidity ^0.8.3;
-import "@opencontracts/contracts/OpenContractAlpha.sol"
 
-contract ExampleContract is OpenContractAlpha("e6295f30c2eb24805ed2a1568935a2db73e0d33e26b2a12a84fbad908251b458") {
-    
-    string enclaveStorage;
-    string userStorage;
-    
-    function saveFromEnclave (bytes32 oracleHash, string memory someString) public {
-        requireOracle(oracleHash);
-        enclaveStorage = someString;
+pragma solidity ^0.8.0;
+
+import "https://github.com/open-contracts/protocol/blob/main/solidity_contracts/OpenContractRopsten.sol";
+
+contract FiatSwap is OpenContractAlpha {
+
+    mapping(bytes32 => address) beneficiary;
+    mapping(bytes32 => uint256) amount;
+
+
+    function EthForHash(bytes32 txHash) public view returns(uint256) {
+        require(msg.sender == beneficiary[txHash], "No Eth offered for you.");
+        return amount[txHash];
     }
 
-    function saveFromUser (string memory someString) public {
-        userStorage = someString;
+
+    function venmoPurchase(bytes32 oracleHash, address payable msgSender, bytes32 txHash) 
+    public _oracle(oracleHash, msgSender, this.venmoPurchase.selector) returns(bool) {
+        require(beneficiary[txHash] == msgSender);
+        uint256 reward = amount[txHash];
+        amount[txHash] = 0;
+        return msgSender.send(reward);
+    }
+
+    function sell(address buyer, bytes32 txHash) public payable {
+        amount[txHash] = msg.value;
+        beneficiary[txHash] = buyer;
     }
 }
