37f8ed885c1c48507bfd3cd6619a7553eb7d0626
open-contracts <90533597+open-contracts@users.noreply.github.com>
2022-01-13 14:55:25 +0100

    Update contract.sol

--- contract.sol
+++ contract.sol
@@ -7,13 +7,13 @@ contract PayTwitterAccount is OpenContractAlpha {
     mapping(string => bool) owner;
     
     constructor (string memory twitterHandle) {
-        owner[twitterHandle] = true;
+        isOwner[twitterHandle] = true;
         setOracle("any", this.claim.selector)
     }
     
     function claim(bytes32 oracleID, string memory twitterHandle, address tweetedAddress)
     public checkOracle(oracleID, this.claim.selector) {
-        require(owner[twitterHandle], "Tweet does not come from the correct twitter handle.");
+        require(isOwner[twitterHandle], "Tweet does not come from the correct twitter handle.");
         
         // transfer all ETH
         payable(tweetedAddress).transfer(address(this).balance);
