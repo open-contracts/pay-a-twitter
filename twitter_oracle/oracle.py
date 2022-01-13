f5c1b4c43e50b2aa56ba05d42b6b6ae4fba9ded4
open-contracts <90533597+open-contracts@users.noreply.github.com>
2022-01-13 15:04:38 +0100

    Update oracle.py

--- twitter_oracle/oracle.py
+++ twitter_oracle/oracle.py
@@ -18,21 +18,21 @@ with opencontracts.enclave_backend() as enclave:
     url = mhtml['Snapshot-Content-Location']
     match = re.match('^https://twitter.com/.*/status/.*', url)
     assert match is not None, 'Need to click on a specific tweet before hitting submit.'
-    user, _, status_id = url.replace('https://twitter.com/', '').split("/")
+    handle, _, status_id = url.replace('https://twitter.com/', '').split("/")
     status_id = int(status_id)
     html = [_ for _ in mhtml.walk() if _.get_content_type() == "text/html"][0]
     parsed = BeautifulSoup(html.get_payload(decode=False))
     title = parsed.find(attrs={'http-equiv': 'origin-trial'}).findNextSibling()
     tweet = title.text.split(': "', 1)[1].strip('" / Twitter')
     unix_time = ((status_id>>22) + 1288834974657)
-    return user, unix_time, tweet
+    return handle, unix_time, tweet
   
-  user, unix_time, tweet = enclave.interactive_session(url='https://twitter.com/home',
+  handle, unix_time, tweet = enclave.interactive_session(url='https://twitter.com/home',
                                                        parser=extract_from_tweet,
                                                        instructions=instructions)
   
-  enclave.print(f"Verified that '{user}' tweeted '{tweet}' at unix time {unix_time}")
+  enclave.print(f"Verified that '{handle}' tweeted '{tweet}' at unix time {unix_time}")
   time.sleep(4)
-  enclave.submit(user, unix_time, tweet,
+  enclave.submit(handle, tweet,
                  types=("string", "uint256", "address"),
                  function_name="submitTweet")
