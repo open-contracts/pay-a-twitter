2f7987f40a10fe3d821e820689e3ae92da85aa62
Open Contracts <90533597+open-contracts@users.noreply.github.com>
2022-02-08 10:39:15 -0800

    Update oracle.py

--- twitter_oracle/oracle.py
+++ twitter_oracle/oracle.py
@@ -5,34 +5,26 @@ import quopri
 
 with opencontracts.enclave_backend() as enclave:
 
-  enclave.print("Twitter Oracle started running in the Enclave!")
-   
+  enclave.print("Twitter Oracle started running in the Enclave! Log into Twitter to prove your handle.")
+
   instructions = f"""
-  1) Login and click on a tweet.
-  2) Click the 'Submit' button on the right.
+  1) Login
+  2) On the Twitter home page, click the 'Submit' button on the right.
   """
   
-  def extract_from_tweet(mhtml):
+  def extract_handle(mhtml):
     mht_string = quopri.decodestring(mhtml.replace("=\n", "")).decode('latin-1')
     mhtml = email.message_from_string(mht_string)
     url = mhtml['Snapshot-Content-Location']
-    match = re.match('^https://twitter.com/.*/status/.*', url)
-    assert match is not None, 'Need to click on a specific tweet before hitting submit.'
-    handle, _, status_id = url.replace('https://twitter.com/', '').split("/")
-    status_id = int(status_id)
+    assert url == "https://twitter.com/home", f"You clicked 'Submit' on '{url}', but should do so on 'https://twitter.com/home'!"
     html = [_ for _ in mhtml.walk() if _.get_content_type() == "text/html"][0]
     parsed = BeautifulSoup(html.get_payload(decode=False))
-    title = parsed.find(attrs={'http-equiv': 'origin-trial'}).findNextSibling()
-    tweet = title.text.split(': "', 1)[1].strip('" / Twitter')
-    unix_time = ((status_id>>22) + 1288834974657)
-    return handle, unix_time, tweet
+    handle = parsed.find(attrs={'aria-label': 'Profile'})['href'].split('/')[-1]
+    return handle
   
-  handle, unix_time, tweet = enclave.interactive_session(url='https://twitter.com/home',
-                                                       parser=extract_from_tweet,
-                                                       instructions=instructions)
+  handle = enclave.interactive_session(url='https://twitter.com/home', 
+                                       parser=extract_handle,
+                                       instructions=instructions)
   
-  enclave.print(f"Verified that '{handle}' tweeted '{tweet}' at unix time {unix_time}")
-  time.sleep(4)
-  enclave.submit(handle, tweet,
-                 types=("string", "uint256", "address"),
-                 function_name="submitTweet")
+  address = enclave.user_input(f"Verified that {enclave.user()} belongs to @{handle}!")
+  enclave.submit(handle, enclave.user(), types=("string", "uint256", "address"), function_name="submitTweet")
